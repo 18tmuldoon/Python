7c1df05aea84398cb197b246678b2da7f9926dd0
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2023-11-16 14:17:05 +0000

    Add files via upload

--- m,m,m.py
+++ m,m,m.py
@@ -1,43 +1,51 @@
-# mean
 file = open("mean-median-mode-frequency.csv", "r")
-t = file.read(129)
-
+f = file.read()
 file.close
 
+t = f[0:129]
+g = f[131:271]
+h = f[273:451]
+
+# mean
 t1 = t.split(",")
-'''
-t1 = t.split(" ")
-t1 = t.split("\n")
-t1 = t.split("'")
-'''
-print(t1)
-#print(t1)
-
 x = len(t1)
-
 x1 = x
-for o in range(0, x):
-    t1[o] = int(t1[o])
 
-
+for a in range(0, x):
+    t1[a] = int(t1[a])
 i = 0
 sum1 = 0
+
 while x > 0:
     sum1 = t1[i] + sum1
     x = x-1
     i = i+1
 
-ans = sum1 / x1
+mean = sum1 / x1
+print("Mean is ", mean)
 
-print(ans)
-'''
-
-file = open("mean-median-mode-frequency.csv", "w")
-file = del(129)
-file.close
 #median
-file = open("mean-median-mode-frequency.csv", "r")
-t = file.read(129)
-print(t)
-file.close
-'''
+g1 = g.split(",")
+g1 = sorted(g1)
+v = len(g1)
+d = v//2
+median = 0
+
+if (v % 2) != 0:
+    median = g1[d]
+elif (v % 2) == 0:
+    median = (d + (d+1)) / 2
+    
+print("Median is " , median)
+
+#mode
+h1 = h.split(",")
+q = len(h1)
+p = 0
+y = 0
+
+while p <= q:
+    if y != h1[y]:
+        print(y)
+        y = y + 1
+    p = p + 1    
