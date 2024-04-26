969fdace61637b0846a1fde2f26bb6fdafb8614e
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2024-04-26 14:36:57 +0100

    Add files via upload

--- Mean,Median,Mode.py
+++ Mean,Median,Mode.py
@@ -1,6 +1,16 @@
 F_data = open("1000 random numbers for frequency.txt", "r")
 C_data = open("1000 random numbers - corrupted 1", "r")
+data = data = C_data.read()
+dataC = []
 
+for word in data:
+    Hash = False
+    Slash = False
+    if data.find("#"):
+    
+    if data.find("/")
+        
+#dataC.append(data)
 
 #While True:
 def Menu():
@@ -12,17 +22,29 @@ def Menu():
     inp = int(input("Enter Selection:"))
     return inp
 
+def mean():
+    print(data)
+
+
+
 inp = Menu()
 if inp == 1:
-    print(C_data.read())
+    mean1 = mean()
+    print("mean is ", mean1)
+    
 elif inp == 2:
+    median1 = median()
     
 elif inp == 3:
+    mode1 = mode()
+    print("Mode is ", mode1)
 
 elif inp == 4:
-    
+    freq1 = freq()
+    print("Frequancy is ", freq1) 
 elif inp == 5:
-    break
+    print("End")
+    #break
 else:
     print("Invalid input")
 inp = Menu()
