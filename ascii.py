7681a15eeb76215fae16caafee96cff9d161ff68
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2024-05-03 14:36:42 +0100

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,26 @@
+character = input("Input a character")
+
+asValue = ord(character)
+print("The ASCII value of ", character, "is: ", asValue)
+
+Uni = int(input("Input a Unicode value"))
+UniValue = chr(Uni)
+print("The Unicode related character is ", UniValue)
+
+message = ("abcdefghijklmonopqrstuvwxyz")
+
+leng = len(message)
+x = 0
+output = []
+
+while leng > 0 :
+    y = ord(message[x])
+    t = y+2
+    if t > 102:
+        
+    q = chr(t)
+    output.append(q)
+    x = x+1
+    leng = leng-1
+    
+print(output)
