dea09e68c24525bd2a66128e3b0b921418eb7c87
18tmuldoon <112865269+18tmuldoon@users.noreply.github.com>
2024-05-13 14:51:06 +0100

    Add files via upload

---
+++ Coding revision.py
@@ -0,0 +1,55 @@
+#1
+lst = []
+for x in range(0 , 5):
+    lst.append(int(input("Input a number")))
+    
+for x in range(0 , len(lst)):
+    lst[x] += 1
+    print(lst)
+
+#2
+hours = [12, 7, 9, 9, 6, 8, 2]
+week = 0
+
+for x in range(0 , len(hours)):
+    week = week + (hours[x] * .5)
+    
+print(week)
+
+cost = week * 1.35
+
+print("Steven drinks ", week, " litres of milk a week")
+print("Steven owes ", cost, " a week")
+
+#3
+msr = []
+for x in range(0 , 7):
+    msr.append(int(input("Input measurments")))
+
+total1 = sum(msr)
+print("The total rainfall for the week is ", total1 , "cm")
+
+for x in range(0 , len(msr)):
+    if msr[x] > 3.5:
+        print("Day ", x , " excedes 3.5cm of rainfall")
+
+
+#4
+nsales = int(input("Input the amount of sales people"))
+names = []
+sales = []
+
+for x in range (0 , nsales):
+    names.append(input("Input the name of employee"))
+    sales.append(int(input("Input the value of this employees sales")))
+
+max1 = max(sales)
+min1 = min(sales)
+total2 = sum(sales)
+avgS = total2 / len(sales)
+
+print("Total sales = ", total2)
+print("Max sales by any one salesperson = ", max1)
+print("Min sales by any one salesperson = ", min1)
+print("Average sales = ", avgS)
+
